7a1869db60d04671f8a69f5a5b54210d0bd3d00b
Jakub Mandula <jakub.aludnam@gmail.com>
2015-01-28 00:09:49 +0100

    set up index file

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function($){
+  function Calendar(year){
+    this.startYear = year
+  }
+
+  Calendar.prototype.populate = function(){
+
+  }
+
+
+  $(document).ready(function(){
+    $(".yearSelect").on("change", function(){
+      console.log($(this).val());
+    });
+  });
+
+
+
+
+})($);
